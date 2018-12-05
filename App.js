c88c1a5494cb9a6a344bdc2e4e674412c846684c
Eugene Neo <neo.eugene@dhs.sg>
2018-12-05 21:31:35 +0800

    Mark Zuckerburg 1.0

--- App.js
+++ App.js
@@ -7,17 +7,17 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Header textIwant={"hello"} /> 
-      <Header textIwant={"goodbyeeee"} />
+      <Header textIwant={"MARK"} /> 
+      <Header textIwant={"Zuckerburg"} />
       <Avatar
   size="xlarge"
   rounded
   source={{uri: "https://specials-images.forbesimg.com/imageserve/5ba415b6a7ea434e4c694719/416x416.jpg?background=000000&cropX1=1503&cropX2=4716&cropY1=21&cropY2=3236"}}
-  onPress={() => console.log("Works!")}
+  onPress={() => console.log("I invade your privacy")}
   activeOpacity={0.7}
 />
-        <Text style={styles.bigblue}>Open up App.js to start working on your app!</Text>
-        <Button title={'Hello'} onPress={()=>{ console.log ('hello') ; }}
+        <Text style={styles.bigblue}>Dating App Profile: Billionaire, Founder of Facebook</Text>
+        <Button title={'Hello'} onPress={()=>{ console.log ('( ͡° ل͜ ͡°)') ; }}
         color="turquoise"
         />
       </View>
--- Components/header.js
+++ Components/header.js
@@ -4,17 +4,35 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 // This is a stateless functional component
 // const Header = (props) => <Text> { props.textIwant }</Text>;
 
-// This is a stateful cfunctional component
+// This is a stateful functional component
 class Header extends React.Component {
 
     constructor(props) {
         super(props);
+
+        // How we create properties
         this.textIwant = props.textIwant;
+        //How we create state
+        this.state = {
+            number: 0 
+        };
+        this.addNumberWhenPressed = this.addNumberWhenPressed.bind(this);
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.number);
+    }
+
+    addNumberWhenPressed() {
+        const previousNumber = this.state.number;
+        this.setState({
+            number: previousNumber + 1
+        });
     }
 
     render() {
         return (
-            <Text>{this.textIwant}</Text>
+            <Text onPress={this.addNumberWhenPressed}>{this.textIwant}</Text>
         );
     }
 }
